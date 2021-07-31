94469dce07f1f27129a3def9a2236fa8fba6a335
oceanacademy <oceanacademypuducherry@gmail.com>
2021-07-31 17:36:42 +0530

    updated books screen

--- lib/screens/BooksScreen.dart
+++ lib/screens/BooksScreen.dart
@@ -29,15 +29,213 @@ class BooksScreen extends StatelessWidget {
               // Generate 100 widgets that display their index in the List.
               children: [
                 Container(
-                  margin: EdgeInsets.all(15),
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(5),
-                    image: DecorationImage(
-                        image: AssetImage('images/rose.jpg'),
-                        fit: BoxFit.cover),
+                  margin: EdgeInsets.all(10),
+                  child: Column(
+                    children: [
+                      Container(
+                        height: 120,
+                        width: 130,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(5),
+                          image: DecorationImage(
+                              image: AssetImage('images/rose.jpg'),
+                              fit: BoxFit.cover,
+                              alignment: Alignment.center),
+                        ),
+                      ),
+                      SizedBox(height: 10),
+                      Text(
+                        'Rose',
+                        style: TextStyle(
+                            color: Colors.black87,
+                            fontSize: 13,
+                            fontFamily: "Ubuntu"),
+                      ),
+                    ],
                   ),
                 ),
-                Text('')
+                Container(
+                  margin: EdgeInsets.all(10),
+                  child: Column(
+                    children: [
+                      Container(
+                        height: 120,
+                        width: 130,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(5),
+                          image: DecorationImage(
+                              image: AssetImage('images/nature1.jpg'),
+                              fit: BoxFit.cover,
+                              alignment: Alignment.center),
+                        ),
+                      ),
+                      SizedBox(height: 10),
+                      Text(
+                        'Little Tree',
+                        style: TextStyle(
+                            color: Colors.black87,
+                            fontSize: 13,
+                            fontFamily: "Ubuntu"),
+                      ),
+                    ],
+                  ),
+                ),
+                Container(
+                  margin: EdgeInsets.all(10),
+                  child: Column(
+                    children: [
+                      Container(
+                        height: 120,
+                        width: 130,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(5),
+                          image: DecorationImage(
+                              image: AssetImage('images/nature2.jpg'),
+                              fit: BoxFit.cover,
+                              alignment: Alignment.center),
+                        ),
+                      ),
+                      SizedBox(height: 10),
+                      Text(
+                        'Nature',
+                        style: TextStyle(
+                            color: Colors.black87,
+                            fontSize: 13,
+                            fontFamily: "Ubuntu"),
+                      ),
+                    ],
+                  ),
+                ),
+                Container(
+                  margin: EdgeInsets.all(10),
+                  child: Column(
+                    children: [
+                      Container(
+                        height: 120,
+                        width: 130,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(5),
+                          image: DecorationImage(
+                              image: AssetImage('images/nature3.jpg'),
+                              fit: BoxFit.cover,
+                              alignment: Alignment.center),
+                        ),
+                      ),
+                      SizedBox(height: 10),
+                      Text(
+                        'Life',
+                        style: TextStyle(
+                            color: Colors.black87,
+                            fontSize: 13,
+                            fontFamily: "Ubuntu"),
+                      ),
+                    ],
+                  ),
+                ),
+                Container(
+                  margin: EdgeInsets.all(10),
+                  child: Column(
+                    children: [
+                      Container(
+                        height: 120,
+                        width: 130,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(5),
+                          image: DecorationImage(
+                              image: AssetImage('images/nature4.png'),
+                              fit: BoxFit.cover,
+                              alignment: Alignment.center),
+                        ),
+                      ),
+                      SizedBox(height: 10),
+                      Text(
+                        'Climate',
+                        style: TextStyle(
+                            color: Colors.black87,
+                            fontSize: 13,
+                            fontFamily: "Ubuntu"),
+                      ),
+                    ],
+                  ),
+                ),
+                Container(
+                  margin: EdgeInsets.all(10),
+                  child: Column(
+                    children: [
+                      Container(
+                        height: 120,
+                        width: 130,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(5),
+                          image: DecorationImage(
+                              image: AssetImage('images/nature5.jpg'),
+                              fit: BoxFit.cover,
+                              alignment: Alignment.center),
+                        ),
+                      ),
+                      SizedBox(height: 10),
+                      Text(
+                        'Location',
+                        style: TextStyle(
+                            color: Colors.black87,
+                            fontSize: 13,
+                            fontFamily: "Ubuntu"),
+                      ),
+                    ],
+                  ),
+                ),
+                Container(
+                  margin: EdgeInsets.all(10),
+                  child: Column(
+                    children: [
+                      Container(
+                        height: 120,
+                        width: 130,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(5),
+                          image: DecorationImage(
+                              image: AssetImage('images/nature6.png'),
+                              fit: BoxFit.cover,
+                              alignment: Alignment.center),
+                        ),
+                      ),
+                      SizedBox(height: 10),
+                      Text(
+                        'Growth',
+                        style: TextStyle(
+                            color: Colors.black87,
+                            fontSize: 13,
+                            fontFamily: "Ubuntu"),
+                      ),
+                    ],
+                  ),
+                ),
+                Container(
+                  margin: EdgeInsets.all(10),
+                  child: Column(
+                    children: [
+                      Container(
+                        height: 120,
+                        width: 130,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(5),
+                          image: DecorationImage(
+                              image: AssetImage('images/rose.jpg'),
+                              fit: BoxFit.cover,
+                              alignment: Alignment.center),
+                        ),
+                      ),
+                      SizedBox(height: 10),
+                      Text(
+                        'Rose',
+                        style: TextStyle(
+                            color: Colors.black87,
+                            fontSize: 13,
+                            fontFamily: "Ubuntu"),
+                      ),
+                    ],
+                  ),
+                ),
               ]),
         ),
       ),
