d31438b6d753f66467af466778541040d640cbe2
KJAYALATHA <k.jayalatha22@gmail.com>
2021-08-07 11:38:18 +0530

    login session changed

--- lib/screens/PdfView.dart
+++ lib/screens/PdfView.dart
@@ -12,9 +12,51 @@ class _PdfViewState extends State<PdfView> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SfPdfViewer.network(
-        '${widget.pdfLink}',
-        // key: _pdfViewerKey,
+      body: Container(
+        height: double.infinity,
+        width: double.infinity,
+        child: CustomScrollView(
+          slivers: [
+            SliverAppBar(
+              floating: false,
+              expandedHeight: 150,
+              // automaticallyImplyLeading: true,
+              toolbarHeight: 50,
+              // stretch: true,
+              pinned: true,
+              collapsedHeight: 50,
+              flexibleSpace: FlexibleSpaceBar(
+                centerTitle: true,
+                title: Text(
+                  'See Pdf',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 15,
+                    fontFamily: 'Ubuntu',
+                    fontWeight: FontWeight.w600,
+                  ),
+                ),
+                background: Image.asset(
+                  'images/background_image.png',
+                  fit: BoxFit.cover,
+                ),
+              ),
+            ),
+            SliverList(
+              delegate:
+                  SliverChildBuilderDelegate((BuildContext context, int index) {
+                return Container(
+                  height: MediaQuery.of(context).size.height,
+                  width: double.infinity,
+                  child: SfPdfViewer.network(
+                    '${widget.pdfLink}',
+                    // key: _pdfViewerKey,
+                  ),
+                );
+              }, childCount: 1),
+            ),
+          ],
+        ),
       ),
     );
   }
