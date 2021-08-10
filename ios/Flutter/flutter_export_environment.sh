8a8ec771f138d9e781e3b3b819245dd40f8009b8
brindakarthik <brindaspringnet@gmail.com>
2021-08-10 16:40:34 +0530

    aug 9 checking 2

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,11 +1,10 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=C:\src\flutter"
-export "FLUTTER_APPLICATION_PATH=C:\Users\kjaya\Documents\flutter\astrology"
+export "FLUTTER_APPLICATION_PATH=E:\issue\astrology"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_TARGET=lib\main.dart"
 export "FLUTTER_BUILD_DIR=build"
-export "SYMROOT=${SOURCE_ROOT}/../build\ios"
 export "FLUTTER_BUILD_NAME=1.0.2"
 export "FLUTTER_BUILD_NUMBER=1.0.2"
 export "DART_OBFUSCATION=false"
