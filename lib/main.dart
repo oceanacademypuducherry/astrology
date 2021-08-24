ef63b1dff4a619c051af176052b37dfe5dacc916
THAMIZH <thamizharasan2373@gmail.com>
2021-08-24 15:50:34 +0530

    main page changed

--- lib/main.dart
+++ lib/main.dart
@@ -44,7 +44,7 @@ void main() async {
   runApp(
     GetMaterialApp(
       debugShowCheckedModeBanner: false,
-      home: Scaffold(body: BottomNavigation()),
+      home: Scaffold(body: MyApp()),
     ),
   );
 }
